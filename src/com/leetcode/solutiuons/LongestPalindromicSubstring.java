4cb803202c7f126512651f92fff68ca2f28ff176
zhaoyang <zy530320582@gmail.com>
2015-07-05 11:03:03 +0800

    新建分支

---
+++ src/com/leetcode/solutiuons/LongestPalindromicSubstring.java
@@ -0,0 +1,22 @@
+package com.leetcode.solutiuons;
+
+/**
+ * Given a string S, find the longest palindromic substring in S. You may assume
+ * that the maximum length of S is 1000, and there exists one unique longest
+ * palindromic substring.
+ * 
+ * @author zhaoyang
+ * 
+ */
+public class LongestPalindromicSubstring {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public String longestPalindrome(String s) {
+    return s;
+  }
+
+}
